b612050fbea0d8c41346d3147422acbe40aaa9ce
Greg V <gvankampen@upei.ca>
2018-05-08 10:58:24 -0300

    basic hello world feature

---
+++ starting_git/Source.cpp
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+// File:  Source.cpp
+// Author: Greg VanKampen
+// This assignment represents my own work and is in accordance with the College Academic Policy
+//
+// Copyright (c) 2018 All Right Reserved by Greg VanKampen
+// Contributors: 
+// Description:  Basic hello world 
+//
+// Date: May 8 2018
+////////////////////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World";
+	getchar();
+}
